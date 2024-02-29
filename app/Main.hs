ae4b8f8ef749341ae4b39bb40a96a604ac4c2173
Bartek Łuka <bartoszluka1@gmail.com>
2024-03-01 00:20:53 +0100

    Persist state of `completed`

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,17 @@
+import Control.Concurrent (newMVar)
 import Foundation
-import Server (application)
 import Network.Wai.Handler.Warp (run)
+import Server (TodoItem (TodoItem), appWithState)
 
+-- main :: IO ()
+-- main = do
+--     let port = 3000
+--     putStrLn $ "listening on port: " <> show port
+--     run port application
+--
 main :: IO ()
 main = do
     let port = 3000
     putStrLn $ "listening on port: " <> show port
-    run port application
+    todos <- newMVar [TodoItem "learn-haskell" "Learn Haskell" False, TodoItem "learn-server" "Learn Server" True]
+    run port $ appWithState todos
--- src/Server.hs
+++ src/Server.hs
@@ -1,9 +1,13 @@
 module Server (
+    TodoItem (..),
     application,
+    appWithState,
 ) where
 
+import Control.Concurrent (modifyMVar, readMVar)
+import Control.Concurrent.MVar (MVar)
+import Data.ByteString qualified as B
 import Data.Text (Text)
-import Data.Text qualified as T
 import Foundation
 import Foundation.Collection (forM_)
 import Network.HTTP.Types (HeaderName, status200)
@@ -12,7 +16,7 @@ import Text.Blaze.Html.Renderer.Utf8 (renderHtml)
 import Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Htmx
-import Prelude qualified
+import Prelude (print)
 
 hello :: Html
 hello = H.docTypeHtml $ do
@@ -39,43 +43,36 @@ contentTypeJavaScript = ("Content-Type", "application/javascript")
 contentTypeCss :: (IsString s) => (HeaderName, s)
 contentTypeCss = ("Content-Type", "text/css")
 
-todos :: Html
-todos = H.docTypeHtml $ do
+todos :: [TodoItem] -> Html
+todos todoList = H.docTypeHtml $ do
     H.head $ do
         H.title "Todos"
         -- styleSheet "style.css"
         htmxScriptTag
     H.body $ do
         h1 "todo list"
-        ul $ forM_ examples (li . todoItem)
-  where
-    examples = [TodoItem "1" "todo1" True, TodoItem "2" "todo2" False]
-
-toText :: Int -> Text
-toText = T.pack . Prelude.show
+        ul $ forM_ todoList (li . todoItem)
 
 data TodoItem = TodoItem
     { identifier :: Text
     , description :: Text
     , completed :: Bool
     }
+    deriving (Show, Eq)
 
 todoItem :: TodoItem -> Html
 todoItem TodoItem{identifier, description, completed} =
     H.div $
         mconcat
             [ H.span (text description)
-            , input ! type_ "checkbox" ! hxPut ("/todos/" <> toValue identifier) ! value "dupa"
+            , input
+                ! type_ "checkbox"
+                ! name "completed"
+                ! hxPut ("/todos/" <> toValue identifier)
+                ! value "true"
+                ! if completed then checked "true" else mempty
             , button ! hxDelete ("/todos/" <> toValue identifier) $ text "delete"
             ]
-
--- <button hx-post="/clicked"
---     hx-trigger="click"
---     hx-target="#parent-div"
---     hx-swap="outerHTML"
--- >
---     Click Me!
--- </button>
 
 application :: Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
 application request respond =
@@ -83,12 +80,7 @@ application request respond =
         ("GET", ["todos"]) ->
             respond $
                 responseLBS status200 [contentTypeHtml] $
-                    renderHtml todos
-        ("GET", ["todos", str]) ->
-            respond $
-                responseLBS status200 [contentTypeHtml] $
-                    renderHtml $
-                        todoItem str
+                    renderHtml $ todos []
         ("GET", ["style.css"]) ->
             respond $
                 responseFile status200 [contentTypeCss] "static/css/style.css" Nothing
@@ -98,8 +90,73 @@ application request respond =
         ("GET", ["favicon.ico"]) ->
             respond $
                 responseFile status200 [contentTypeJavaScript] "static/favicon.ico" Nothing
+        ("PUT", ["todos", todoId]) -> do
+            print todoId
+            rqBody <- getRequestBodyChunk request
+            let newTodo = case rqBody of
+                    "" -> TodoItem{identifier = todoId, description = "dupa", completed = False}
+                    str
+                        | "completed=" `B.isPrefixOf` str ->
+                            TodoItem{identifier = todoId, description = "dupa", completed = True}
+                    _ -> error "dupa"
+
+            print newTodo
+            respond $
+                responseLBS status200 [("Content-Type", "text/plain")] ""
+        ("DELETE", ["todos", todoId]) -> do
+            Prelude.print todoId
+            rqBody <- getRequestBodyChunk request
+            let newTodo = case rqBody of
+                    "" -> TodoItem{identifier = todoId, description = "dupa", completed = False}
+                    str
+                        | "completed=" `B.isPrefixOf` str ->
+                            TodoItem{identifier = todoId, description = "dupa", completed = True}
+                    _ -> error "dupa"
+
+            Prelude.print newTodo
+            respond $
+                responseLBS status200 [("Content-Type", "text/plain")] ""
         other -> do
             Prelude.print other
+            rqBody <- getRequestBodyChunk request
+            Prelude.print rqBody
             respond $
                 responseLBS status200 [contentTypeHtml] $
                     renderHtml hello
+
+appWithState :: MVar [TodoItem] -> Request -> (Response -> IO b) -> IO b
+appWithState todoList request respond = do
+    case (requestMethod request, pathInfo request) of
+        ("GET", ["todos"]) -> do
+            todoList' <- readMVar todoList
+            respond $
+                responseLBS status200 [contentTypeHtml] $
+                    renderHtml $
+                        todos todoList'
+        ("GET", ["style.css"]) ->
+            respond $
+                responseFile status200 [contentTypeCss] "static/css/style.css" Nothing
+        ("GET", ["htmx.min.js"]) ->
+            respond $
+                responseFile status200 [contentTypeJavaScript] "static/htmx-v1.9.10-min.js" Nothing
+        ("GET", ["favicon.ico"]) ->
+            respond $
+                responseFile status200 [contentTypeJavaScript] "static/favicon.ico" Nothing
+        ("PUT", ["todos", todoId]) -> do
+            rqBody <- getRequestBodyChunk request
+            let isCompleted = case rqBody of
+                    "" -> False
+                    str | "completed=" `B.isPrefixOf` str -> True
+                    _ -> error "dupa"
+
+            modifyMVar todoList $ \todoList' -> do
+                let changeCompleted item' =
+                        if identifier item' == todoId
+                            then item'{completed = isCompleted}
+                            else item'
+                    new = changeCompleted <$> todoList'
+
+                responseReceived <- respond $ responseLBS status200 [("Content-Type", "text/plain")] ""
+                return (new, responseReceived)
+        -- print todoList
+        _ -> undefined
